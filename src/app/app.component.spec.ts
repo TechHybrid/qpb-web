73b2d22c884e08a0f6af1c76c35e01c349c5f863
Sharad Purohit <sharad.purohit@ge.com>
2017-03-11 00:19:46 +0530

    Changed unit test strings

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -17,16 +17,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   }));
 
-  it(`should have as title 'app works!'`, async(() => {
+  it(`should have as title 'Question Paper Builder'`, async(() => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('app works!');
+    expect(app.title).toEqual('Question Paper Builder');
   }));
 
   it('should render title in a h1 tag', async(() => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('app works!');
+    expect(compiled.querySelector('h1').textContent).toContain('Question Paper Builder');
   }));
 });
